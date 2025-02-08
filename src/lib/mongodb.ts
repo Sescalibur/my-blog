8869b507a348a7677fd7aafd7a65597a33b8da3b
Samet Emin Sevil <sameteminsevil@gmail.com>
2025-02-08 17:57:12 +0300

    again

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,10 +1,7 @@
 import mongoose from 'mongoose'
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local')
-}
 
-const uri = process.env.MONGODB_URI as string
+const uri = process.env.MONGODB_URI
 
 let clientPromise: Promise<typeof mongoose>
 
